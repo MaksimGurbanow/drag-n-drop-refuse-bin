76fa09cdf9a7db10c66ce21f924508b7cb3503ba
Maxim <maksim20051708@gmail.com>
2023-01-10 14:19:25 +0300

    message

--- script.js
+++ script.js
@@ -1,14 +1,33 @@
 const item = document.querySelector('.item')
 const placeholders = document.querySelectorAll('.placeholder')
+const OtherPlaceholder = [placeholders[0], placeholders[1]]
+const hiddenText = document.querySelector('.hint')
 
 item.addEventListener('dragstart', dragstart)
 item.addEventListener('dragend', dragend)
 
 for(const placeholder of placeholders) {
+
     placeholder.addEventListener('dragover', dragover)
     placeholder.addEventListener('dragenter', dragenter)
     placeholder.addEventListener('dragleave', dragleave)
     placeholder.addEventListener('drop', dragdrop)
+    placeholders[2].addEventListener('drop', () =>{
+      placeholders[2].classList.add('deleter-done')
+      item.classList.add('ItemDeleted')
+      hiddenText.classList.remove('hidden')
+    })
+
+    for( const placeholder of OtherPlaceholder){
+      placeholder.addEventListener('drop', () =>{
+      placeholders[2].classList.remove('deleter-done')
+      placeholders[2].classList.add('deleter')
+    })
+    placeholders[2].addEventListener('dblclick', () =>{
+      item.classList.remove('ItemDeleted')
+      hiddenText.classList.add('hidden')
+    })
+    }
 }
 
 function dragstart(event){
@@ -17,7 +36,7 @@ function dragstart(event){
 }
 
 function dragend(event){
-    event.target.className = 'item'
+    event.target.classList.remove('hold', 'hide')
     
 }
 
